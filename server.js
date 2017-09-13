965d0d291b2ab738883e78d466de1cf414d11e0a
Dan Murphy <d.p.murph19@gmail.com>
2017-09-12 21:39:50 -0600

    changed server to use wildcard so all paths serve index

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
