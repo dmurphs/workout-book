519029efd0a52b3e0b4a92a48e969a8978ccbe01
Dan Murphy <d.p.murph19@gmail.com>
2017-09-13 21:41:38 -0600

    fixed issue with hard requests and made build automatically happen on install for deployments

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var app = express();
 var path = require('path');
+
+var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/dist')));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(app.get('port'), function() {
